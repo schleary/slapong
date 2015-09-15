be3a841dc04a2062826703b4ff258c92036f3183
Holly Leary <schleary@gmail.com>
2015-09-14 18:48:03 -0700

    admin works independently of non-admin user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,29 +9,20 @@ class ApplicationController < ActionController::Base
 
   # # Any more helpers? Add signed_in helper?
   helper_method :current_user
-
   helper_method :admin?
-  #
-  # protected
-  #
-  # def admin?
-  #   session[:email] == 'schleary@gmail.com'
-  # end
-  #
-  # def authorize
-  #   unless admin?
-  #     flash[:error] = "unauthorized access"
-  #     redirect_to home_path
-  #     false
-  #   end
-  # end
+
+  protected
+
+  def admin?
+    current_user[:email] == 'schleary@gmail.com'
+  end
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  
+
 
   def authorize
     redirect_to login_url, alert: "Not authorized" if current_user.nil?
